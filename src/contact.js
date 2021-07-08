a818981b4eab7d21ae5be50dc5d0f15ef26f94ce
Iris <iclinde@xs4all.nl>
2021-07-08 15:49:21 +0200

    started on contactpage

--- src/contact.js
+++ src/contact.js
@@ -1,14 +1,76 @@
 export function createContactpage () {
+    const d = new Date();
+
     const contentDiv = document.getElementById("content");
 
     const contactPge = document.createElement('div');
     contactPge.classList.add('tab3');
     contactPge.setAttribute('id', 'contactpage');
 
-    const homeHeading = document.createElement('h2');
-    homeHeading.textContent = "Contact info!";
+    const contactHeading = document.createElement('h2');
+    contactHeading.textContent = "Contact info!";
 
-    contactPge.appendChild(homeHeading);
+    contactPge.appendChild(contactHeading);
+
+    const contactSpecial = document.createElement('div');
+    contactSpecial.classList.add('openinghours');
+
+    const openingTimes = document.createElement('h2');
+    openingTimes.innerHTML = `Opening hours`;
+
+    const openSpecial = document.createElement('div');
+    openSpecial.setAttribute('style', 'text-align: end; width: max-content; margin: auto;');
+
+    const openText1 = document.createElement('p');
+    openText1.textContent = 'Monday: 16:00 - 23:00';
+
+    const openText2 = document.createElement('p');
+    openText2.textContent = 'Tuesday: 16:00 - 23:00';
+
+    const openText3 = document.createElement('p');
+    openText3.textContent = 'Wednesday: 16:00 - 23:00';
+
+    const openText4 = document.createElement('p');
+    openText4.textContent = 'Thurday: 16:00 - 23:00';
+
+    const openText5 = document.createElement('p');
+    openText5.textContent = 'Friday: 14:00 - 23:00';
+
+    const openText6 = document.createElement('p');
+    openText6.textContent = 'Saturday: 12:00 - 23:00';
+
+    const openText7 = document.createElement('p');
+    openText7.textContent = 'Sunday: 12:00 - 23:00';
+
+    if(d.getDay() == 1) {
+        openText1.style.fontWeight = "bold";
+    } else if(d.getDay() == 2) {
+        openText2.style.fontWeight = "bold";
+    } else if(d.getDay() == 3) {
+        openText3.style.fontWeight = "bold";
+    } else if(d.getDay() == 4) {
+        openText4.style.fontWeight = "bold";
+    } else if(d.getDay() == 5) {
+        openText5.style.fontWeight = "bold";
+    } else if(d.getDay() == 6) {
+        openText6.style.fontWeight = "bold";
+    } else if(d.getDay() == 0) {
+        openText7.style.fontWeight = "bold";
+    } else { 
+        console.log("cannot find day, contact");
+    }
+
+    contactPge.appendChild(contactHeading);
+    contactSpecial.appendChild(openingTimes);
+    openSpecial.appendChild(openText1);
+    openSpecial.appendChild(openText2);
+    openSpecial.appendChild(openText3);
+    openSpecial.appendChild(openText4);
+    openSpecial.appendChild(openText5);
+    openSpecial.appendChild(openText6);
+    openSpecial.appendChild(openText7);
+    contactSpecial.appendChild(openSpecial);
+    contactPge.appendChild(contactSpecial);
 
     contentDiv.appendChild(contactPge);
 
